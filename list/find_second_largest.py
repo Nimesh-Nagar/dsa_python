7f367024c7097c43eabeb20f38a61468b44b4b68
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-02-06 00:21:22 +0530

    second largest in array

--- list/find_second_largest.py
+++ list/find_second_largest.py
@@ -1,24 +1,55 @@
-# Find secind largest element 
+# Find the second largest element from the list 
 
-def second_largest_ele(arr):
+# [Method 1] : Using two loops time complexity : theta(n) 
+def get_second_max(arr):
 
-    if not lst:
+    if len(arr) <= 1:
         return None
     
-    else:
-        first_large = arr[0]
-        second_large = None 
+    first_lar = getmax(arr)
+    second_lar = None 
+
+    for x in arr:
+        if x != first_lar:
+            if second_lar == None:
+                second_lar = x 
+            else:
+                second_lar = max(second_lar, x)
+
+    return second_lar
+    
+def getmax(lst):
+    large = lst[0]
+    for ele in range(1, len(lst)):
+        if lst[ele] > large:
+            large = lst[ele]
+    return large
+
+# ---------------------------------------------------------------------
+# Method 2 : using only one loop 
+
+def second_max(arr):
+
+    if len(arr) <= 1:
+        return None
+    
+    lar = arr[0]
+    slar = None 
+
+    for x in arr[1:]:
+
+        if x > lar:
+            slar = lar
+            lar = x 
         
-        for i in range(1, len(arr)):
-            if arr[i] > first_large:
-                second_large = first_large
-                first_large = arr[i]
-            else: 
-                second_large = arr[i]
+        elif x != lar:
+            if slar == None or slar < x:
+                slar = x
 
-        return second_large
+    return slar
 
-lst = [10,5,20,8]
-arr = [12, 35, 1, 10, 34, 1]
+arr = [10,5,20,8]
+# arr = [5, 20, 12, 10, 20, 10, 12]
 
-print("Largest element in list : ",second_largest_ele(arr))
+# print("Largest element in list : ",get_second_max(arr))
+print("Second Largest element is : ",second_max(arr))
