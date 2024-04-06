8f8cbc6745c0f683337b6907098891287cef8d43
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-04-06 17:20:14 +0530

    problems

---
+++ searching/q1_first_occurance.py
@@ -0,0 +1,46 @@
+# find the index of first occurance of number 
+
+###################### Linear Search Approch ######################
+def first_occurance(arr, x):
+    
+    for idx in range(0, len(arr)):
+        if (arr[idx] == x):
+            return idx
+        
+    return -1
+
+###################### Binary Search Approch ######################
+
+def first_occurance_b(arr, x):
+    low = 0
+    high = len(arr) - 1
+    
+    while (low <= high):
+        mid = (low + high)//2
+    
+        if arr[mid] > x:
+            high = mid - 1 
+            
+        elif arr[mid] < x: 
+            low = mid + 1
+            
+        else:
+            if (mid == 0 or arr[mid-1] != arr[mid]):
+                return mid
+            else:
+                high = mid - 1 
+                            
+    return -1 
+
+
+arr = [5, 10, 10, 20, 20, 40]
+num = 20
+
+idx = first_occurance(arr, num)
+print(idx)
+
+print(first_occurance_b(arr, num))
+
+
+
+
---
+++ searching/q2_last_occurance.py
@@ -0,0 +1,10 @@
+
+###################### Linear Search Approch ######################
+def last_occurance(arr, n):
+    pass
+
+arr = [5, 10, 10, 20, 20, 40]
+num = 20
+
+idx = last_occurance(arr, num)
+print(idx)
