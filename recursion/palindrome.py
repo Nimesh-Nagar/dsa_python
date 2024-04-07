f211d099ff9bdaa9bf56363e48cdc330931356a2
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-04-07 12:47:48 +0530

    palindrome using recursion

---
+++ recursion/palindrome.py
@@ -0,0 +1,28 @@
+# A recursive Python program to check whether a given number is palindrome or not
+
+def isPalRec(st, s, e) :
+	
+	if (s == e):
+		return True
+
+	if (st[s] != st[e]) :
+		return False
+
+	if (s < e + 1) :
+		return isPalRec(st, s + 1, e - 1);
+
+	return True
+
+def isPalindrome(st) :
+	n = len(st)
+	if (n == 0) :
+		return True
+	
+	return isPalRec(st, 0, n - 1);
+
+
+st = "abeba"
+if (isPalindrome(st)) :
+	print("Yes")
+else :
+	print("No")
