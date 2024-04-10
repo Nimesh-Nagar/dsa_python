2c61dc6d4fc1f9d396ee3922c602da4e013d4c4e
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-04-10 22:38:00 +0530

    finds last occurance

--- searching/q2_last_occurance.py
+++ searching/q2_last_occurance.py
@@ -1,10 +1,42 @@
 
 ###################### Linear Search Approch ######################
 def last_occurance(arr, n):
-    pass
+    for idx in reversed(range(len(arr))):
+        if arr[idx] == n:
+            return idx
+    return -1
+
+
+###################### Binary Search Approch ######################
+
+def last_occur_b(arr, n):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high)//2
+
+        if n > arr[mid]:
+            low = mid + 1
+        elif n < arr[mid]:
+            high = mid + 1
+
+        else:
+            if arr[mid] == len(arr) or arr[mid] != arr[mid+1]:
+                return mid
+            else:
+                low = mid + 1
+
+
 
 arr = [5, 10, 10, 20, 20, 40]
 num = 20
 
-idx = last_occurance(arr, num)
-print(idx)
+# index = last_occurance(arr, num)
+
+index = last_occur_b(arr, num)
+
+if index >= 0:
+    print(f"Last occurance of Number {num} found at index {index}")
+else:
+    print(f"Number {num} not in array")
