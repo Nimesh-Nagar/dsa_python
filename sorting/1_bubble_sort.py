eae351fba9fca1696977b0a4d859e9b648a30c3e
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-04-22 12:26:11 +0530

    bubble sort algorithm

--- sorting/1_bubble_sort.py
+++ sorting/1_bubble_sort.py
@@ -3,12 +3,12 @@
 def bubble_sort(lst):
     n = len(lst)
 
-    for i in range(n-1):
-        for j in range(n-i-1):
-            if lst[j] > lst[j+1]:
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if lst[j] > lst[j + 1]:
                 temp = lst[j]
-                lst[j] = lst[j+1]
-                lst[j+1] = temp 
+                lst[j] = lst[j + 1]
+                lst[j + 1] = temp
 
                 # lst[j], lst[j+1] = lst[j+1], lst[j]
 
