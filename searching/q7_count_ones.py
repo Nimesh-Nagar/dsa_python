5129fa9ad45f76e6b59278c4aa3716e9f7181643
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-04-26 11:07:05 +0530

    count number of ones in list

---
+++ searching/q7_count_ones.py
@@ -0,0 +1,17 @@
+"""
+Given a binary array arr[], which is sorted in non-increasing order, 
+count the number of 1's in it. 
+""" 
+
+def count_ones(arr):
+   pass 
+        
+
+arr = [1, 1, 1, 1, 0, 0]
+# arr = [0, 0, 1, 1, 1, 1]
+ 
+print(count_ones(arr))
+
+
+
+
