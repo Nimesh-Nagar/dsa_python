3fe6006d1398aafb43b75a7ff39eb4e81a0aa8fc
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-06-05 13:49:46 +0530

    Quick Sort using lomuto partition technique

---
+++ sorting/5_quick_sort/5.4_quick_sort.py
@@ -0,0 +1,32 @@
+# Quick sort using Lomuto Partition technique
+
+def lomuto_partition(arr, start, end):
+    pivot = arr[end]
+    idx = start - 1 
+
+    for itr in range(start,end):
+        if arr[itr] <= pivot:
+            idx += 1 
+            arr[idx], arr[itr] = arr[itr], arr[idx]
+
+    arr[idx+1], arr[end] = arr[end], arr[idx+1]
+
+    return idx + 1 
+ 
+
+def quick_sort(arr, start, end):
+    if start < end:
+        pivot_idx = lomuto_partition(arr, start, end)
+        quick_sort(arr, start, pivot_idx - 1)
+        quick_sort(arr, pivot_idx + 1, end)
+
+# unsorted_arr = [10, 80, 90, 40, 50, 70]
+unsorted_arr = [8, 4, 7, 9, 3, 10, 5]
+low  = 0
+high = len(unsorted_arr) - 1 
+
+
+print("Before : ",unsorted_arr)
+quick_sort(unsorted_arr, low, high)
+
+print("After : ",unsorted_arr)
