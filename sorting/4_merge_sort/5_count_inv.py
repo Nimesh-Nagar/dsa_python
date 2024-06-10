955e41757442e4f6d6941db830b2cd4b8e16c019
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-06-10 12:01:28 +0530

    count inversion using merge sort algo

--- sorting/4_merge_sort/5_count_inv.py
+++ sorting/4_merge_sort/5_count_inv.py
@@ -1,63 +1,62 @@
 # Python 3 program to count inversions in an array
 
+
 def mergeSort(arr, n):
-	temp_arr = [0]*n
-	return _mergeSort(arr, temp_arr, 0, n-1)
+    temp_arr = [0] * n
+    return _mergeSort(arr, temp_arr, 0, n - 1)
+
 
 def _mergeSort(arr, temp_arr, left, right):
 
-	inv_count = 0
+    inv_count = 0
 
-	if left < right:
+    if left < right:
 
-		mid = (left + right)//2
+        mid = (left + right) // 2
 
-		inv_count += _mergeSort(arr, temp_arr,
-								left, mid)
+        inv_count += _mergeSort(arr, temp_arr, left, mid)
 
-		inv_count += _mergeSort(arr, temp_arr,
-								mid + 1, right)
+        inv_count += _mergeSort(arr, temp_arr, mid + 1, right)
 
-
-		inv_count += merge(arr, temp_arr, left, mid, right)
-	return inv_count
+        inv_count += merge(arr, temp_arr, left, mid, right)
+    return inv_count
 
 
 def merge(arr, temp_arr, left, mid, right):
-	i = left	 
-	j = mid + 1 
-	k = left	 
-	inv_count = 0
+    i = left
+    j = mid + 1
+    k = left
+    inv_count = 0
 
-	while i <= mid and j <= right:
+    while i <= mid and j <= right:
 
-		if arr[i] <= arr[j]:
-			temp_arr[k] = arr[i]
-			k += 1
-			i += 1
-		else:
-			temp_arr[k] = arr[j]
-			inv_count += (mid-i + 1)
-			k += 1
-			j += 1
+        if arr[i] <= arr[j]:
+            temp_arr[k] = arr[i]
+            k += 1
+            i += 1
+        else:
+            temp_arr[k] = arr[j]
+            inv_count += mid - i + 1
+            k += 1
+            j += 1
 
-	while i <= mid:
-		temp_arr[k] = arr[i]
-		k += 1
-		i += 1
+    while i <= mid:
+        temp_arr[k] = arr[i]
+        k += 1
+        i += 1
 
-	while j <= right:
-		temp_arr[k] = arr[j]
-		k += 1
-		j += 1
+    while j <= right:
+        temp_arr[k] = arr[j]
+        k += 1
+        j += 1
 
-	for loop_var in range(left, right + 1):
-		arr[loop_var] = temp_arr[loop_var]
+    for loop_var in range(left, right + 1):
+        arr[loop_var] = temp_arr[loop_var]
 
-	return inv_count
+    return inv_count
 
 
-arr = [1, 20, 6, 4, 5]
+arr = [1, 20, 6, 4, 5, 2]
 n = len(arr)
 result = mergeSort(arr, n)
 print("Number of inversions are", result)
