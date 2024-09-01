9e5541a3d83b423319cedf63ccf9bc0ad4b28259
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-09-01 20:47:26 +0530

    reverse words in string

---
+++ string/8_reverse_words_str.py
@@ -0,0 +1,48 @@
+'''
+Reverse words in a given string
+Let the input string be “i like this program very much”. 
+The function should change the string to “much very program this like i”
+'''
+
+#-----------------------------------
+# METHOD 1 
+#-----------------------------------
+org_str = "Welcome to dsa"
+print(org_str)
+
+# temp = org_str.split(" ")
+# print(" ".join(temp[::-1]))
+
+print( " ".join(org_str.split(" ")[::-1]) )
+
+
+print('-----------------------------------')
+# METHOD 2
+print('-----------------------------------')
+
+def reverse_words(org_str, start, end):
+    while start < end:
+        org_str[start], org_str[end] = org_str[end], org_str[start]
+        start += 1
+        end -= 1
+
+org_str = "I love programming"
+# org_str = "Welcome to dsa"
+print(org_str)
+
+org_str = list(org_str)
+start = 0 
+
+while True:
+    try:
+        end = org_str.index(' ', start)
+        reverse_words(org_str, start, end-1)
+        start = end + 1
+    
+    except ValueError:
+        reverse_words(org_str, start, len(org_str)-1)
+        break
+
+org_str.reverse()
+org_str = "".join(org_str)
+print(org_str)
