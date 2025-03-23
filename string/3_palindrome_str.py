87256a36d50b4a97ad9f50b49715bd4d874987f2
Nimesh-Nagar <n.nimesh02@gmail.com>
2025-03-24 00:31:54 +0530

    follow coding practice

--- string/3_palindrome_str.py
+++ string/3_palindrome_str.py
@@ -24,12 +24,45 @@ def isPalindrome(str):
 
     return rev == str 
 
-# str = "malayalam"
-str = "geeks"
+print("----- Naive Technique -----")
+print(isPalindrome("malayalam"))
+print(isPalindrome("geeks"))
 
-ans = isPalindrome(str)
+'''
+Time complexity : O(n)
+space complexity : O(n) (new reversed string is crested)
+'''
 
-if ans:
-    print("YES")
-else:
-    print("NO")
+# -------------- (Two Pointer Technique) ----------
+
+def is_palindrome(str):
+    left = 0 
+    right = len(str) - 1
+
+    while left <= right:
+        if str[left] != str[right]:
+            return False
+        else:
+            left += 1
+            right -= 1
+
+    return True
+
+print("---- Using Two Pointer Technique (Optimised) ------ ")
+print(is_palindrome("radar"))
+print(is_palindrome("geeks"))
+
+
+# ------- using Recurion ------
+
+def is_palindrome(s):
+    if len(s) <= 1:  # Base case: single character or empty string
+        return True
+    if s[0] != s[-1]:  # Compare first and last characters
+        return False
+    return is_palindrome(s[1:-1])  # Recur for the remaining substring
+
+# Example Usage:
+print("---- Using Recrsion ----")
+print(is_palindrome("madam"))  
+print(is_palindrome("hello"))  
