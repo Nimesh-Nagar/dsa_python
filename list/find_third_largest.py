762b202819febcb95b84ff343d533aef892338a8
Nimesh-Nagar <n.nimesh02@gmail.com>
2025-05-21 23:38:56 +0530

    third largest number

--- list/find_third_largest.py
+++ list/find_third_largest.py
@@ -7,7 +7,7 @@ def third_largest(arr):
     if size < 3:
         return -1
     
-    first = second = third = float('-inf')
+    first = second = third = -1
 
     for ele in arr:
         if ele > first:
@@ -22,9 +22,9 @@ def third_largest(arr):
         elif second > ele > third:
             third = ele
 
-    return first if third == float('-inf') else third
+    return third
 
 print(third_largest([10, 20, 4, 45, 99, 99, 33]))  # Output: 33
-print(third_largest([5, 5, 5]))  # Output: 5
-print(third_largest([1, 2]))  # Output: 2
-print(third_largest([3, 3, 1, 2]))  # Output: 1
+print(third_largest([5, 5, 5]))  # Output: -1
+print(third_largest([1, 2]))  # Output: -1
+print(third_largest([6, 6, 3, 2]))  # Output: 2s
